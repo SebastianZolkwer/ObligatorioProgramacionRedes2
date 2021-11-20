b0ad44f84897e34bd0ca36760ab2eb8e654596a7
Alex <alexpiczenik@gmail.com>
2021-11-20 13:49:04 -0300

    Agrego logConsumer

--- Obligatorio2_ProgdeRedes_222841_231093/Codigo_Release/University.NetworkProgramming/BusinessLogic/Logic/LogLogic.cs
+++ Obligatorio2_ProgdeRedes_222841_231093/Codigo_Release/University.NetworkProgramming/BusinessLogic/Logic/LogLogic.cs
@@ -3,6 +3,7 @@ using Bussiness.Domain;
 using DataAccessInterface;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BusinessLogic.Logic
@@ -16,7 +17,20 @@ namespace BusinessLogic.Logic
 
         public List<Log> GetAll(string userName, string gameTitle, string date)
         {
-           
+            List<Log> _logs = _logRepository.GetAll();
+            if (!String.IsNullOrEmpty(userName))
+            {
+                _logs = _logs.Where(log => log.clientName.Contains(userName)).ToList();
+            }
+            if (!String.IsNullOrEmpty(gameTitle))
+            {
+                _logs = _logs.Where(log => log.gameTitle.Contains(gameTitle)).ToList();
+            }
+            if (!String.IsNullOrEmpty(date))
+            {
+                _logs = _logs.Where(log => log.date.Contains(date)).ToList();
+            }
+            return _logs;
         }
     }
 }
--- Obligatorio2_ProgdeRedes_222841_231093/Codigo_Release/University.NetworkProgramming/WebApiLogs/Program.cs
+++ Obligatorio2_ProgdeRedes_222841_231093/Codigo_Release/University.NetworkProgramming/WebApiLogs/Program.cs
@@ -1,7 +1,9 @@
+using LogProducerConsumer;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,12 @@ namespace WebApiLogs
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            LogConsumer.DeclareQueue(channel);
+            LogConsumer.ReceiveMessages(channel);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
