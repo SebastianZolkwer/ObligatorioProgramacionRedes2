150a35748f892d3be76d933c6495ee25a86f2677
Sebastian Zolkwer <sebastianzolkwer@gmail.com>
2021-11-22 19:12:48 -0300

    Borramos metodos innecesarios

--- Obligatorio2_ProgdeRedes_222841_231093/Codigo_Release/University.NetworkProgramming/WebApiLogs/LogConsumer.cs
+++ Obligatorio2_ProgdeRedes_222841_231093/Codigo_Release/University.NetworkProgramming/WebApiLogs/LogConsumer.cs
@@ -10,17 +10,10 @@ namespace LogProducerConsumer
 {
     public class LogConsumer
     {
-        private const string SimpleQueueName = "n6aBasicQueue";
-
-
+        private const string SimpleQueueName = "BasicQueue";
 
         public static void DeclareQueue(IModel channel)
         {
-            //Name (queue name)
-            //Durable(the queue will survive a broker restart)
-            //Exclusive(used by only one connection and the queue will be deleted when that connection closes)
-            //Auto - delete(queue that has had at least one consumer is deleted when last consumer unsubscribes)
-            //Arguments(optional; used by plugins and broker - specific features such as message TTL, queue length limit, etc)
 
             channel.QueueDeclare(
                 queue: SimpleQueueName,
